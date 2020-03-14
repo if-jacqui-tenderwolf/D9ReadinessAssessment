8e89762fd96f9527c9208e61d8fd029c19ed1ea6
Lisa Ridley <lisa@ifsight.com>
2020-03-14 10:23:56 -0400

    Tweaking post update/install composer script to add checks for files before changing permissions

--- scripts/trinit.sh
+++ scripts/trinit.sh
@@ -3,24 +3,22 @@
 # Initialization script for TabRasa8 project
 
 # Copy the settings.php file to docroot/sites/default/settings.php
-chmod 0777 ./docroot/sites/default/settings.php
+if [ -f ./docroot/sites/default/settings.php]; then
+	chmod 0777 ./docroot/sites/default/settings.php
+fi
 if [ ! -f "docroot/sites/default/settings.php" ]; then
 	cp ./drupal/settings.php ./docroot/sites/default/settings.php
 fi
 chmod 0644 ./docroot/sites/default/settings.php
 
 # Copy the settings.local.php file to docroot/sites/default/settings.php
-chmod 0777 ./docroot/sites/default/settings.local.php
+if [ -f ./docroot/sites/default/settings.local.php ]; then
+	chmod 0777 ./docroot/sites/default/settings.local.php
+fi
 if [ ! -f "docroot/sites/default/settings.local.php" ]; then
 	cp ./drupal/settings.local.php ./docroot/sites/default/settings.local.php
 fi
 chmod 0644 ./docroot/sites/default/settings.local.php
-
-# Create the custom modules and themes directories
-mkdir -p ./docroot/modules/custom
-touch ./docroot/modules/custom/.gitkeep
-mkdir -p ./docroot/themes/custom
-touch ./docroot/themes/custom/.gitkeep
 
 # Remove the examples folder from plupload, which is a security risk
 if [ -d "docroot/libraries/plupload" ]; then
